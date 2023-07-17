8dc853c06a9032c60956f5b7b2f17a2202fbbd72
Mohammed Aljaroudi <maljaroudi@sandiego.edu>
2023-07-16 18:15:30 -0700

    Dictionary API

---
+++ src/routes/api/+server.ts
@@ -0,0 +1,23 @@
+import type { WordDef } from '$lib/types'
+import { json } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+import { DICT_KEY } from '$env/static/private'
+
+export const GET: RequestHandler = async ({ url: { searchParams } }) => {
+	const phrase = searchParams.get('phrase')
+	if (!phrase || phrase.length < 2) return json({ phrase: '' })
+
+	return json(await callAPI(phrase))
+}
+
+async function callAPI(phrase: string): Promise<WordDef[]> {
+	return fetch(
+		`https://www.dictionaryapi.com/api/v3/references/learners/json/${phrase}?key=${DICT_KEY}`
+	)
+		.then(res => res.json() as Promise<WordDef[]>)
+		.then(def => def)
+		.catch(err => {
+			console.error('callAPI', err)
+			return []
+		})
+}
