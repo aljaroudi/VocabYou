ee0ec42cc8b872761f5a9482a1df15fcad92bf7c
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-04-07 17:39:54 +0300

    refactor: clean up service worker code by removing unnecessary TypeScript error comments

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,4 @@
 /// <reference types="@sveltejs/kit" />
-/// <reference no-default-lib="true"/>
-/// <reference lib="esnext" />
-/// <reference lib="webworker" />
 import { build, files, version } from '$service-worker'
 
 // Create a unique cache name for this deployment
@@ -19,7 +16,6 @@ self.addEventListener('install', event => {
 		await cache.addAll(ASSETS)
 	}
 
-	// @ts-expect-error waitUntil is not available on type Event
 	event.waitUntil(addFilesToCache())
 })
 
@@ -31,17 +27,14 @@ self.addEventListener('activate', event => {
 		}
 	}
 
-	// @ts-expect-error waitUntil is not available on type Event
 	event.waitUntil(deleteOldCaches())
 })
 
 self.addEventListener('fetch', event => {
 	// ignore POST requests etc
-	// @ts-expect-error request is not available on type Event
 	if (event.request.method !== 'GET') return
 
 	async function respond() {
-		// @ts-expect-error request is not available on type Event
 		const url = new URL(event.request.url)
 		const cache = await caches.open(CACHE)
 
@@ -57,7 +50,6 @@ self.addEventListener('fetch', event => {
 		// for everything else, try the network first, but
 		// fall back to the cache if we're offline
 		try {
-			// @ts-expect-error request is not available on type Event
 			const response = await fetch(event.request)
 
 			// if we're offline, fetch can return a value that is not a Response
@@ -67,13 +59,11 @@ self.addEventListener('fetch', event => {
 			}
 
 			if (response.status === 200) {
-				// @ts-expect-error request is not available on type Event
 				cache.put(event.request, response.clone())
 			}
 
 			return response
 		} catch (err) {
-			// @ts-expect-error request is not available on type Event
 			const response = await cache.match(event.request)
 
 			if (response) {
@@ -85,6 +75,6 @@ self.addEventListener('fetch', event => {
 			throw err
 		}
 	}
-	// @ts-expect-error respondWith is not available on type Event
+
 	event.respondWith(respond())
 })
