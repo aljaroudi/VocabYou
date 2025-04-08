9629eb9f17f3cfc982c7d0855f0c8718d292afa1
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-04-08 13:17:56 +0300

    refactor: simplify API response handling by replacing define and translate functions with getPhrase in +server.ts

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,28 +1,11 @@
-import type { APIResponse, WordDef } from '$lib/types'
 import { json } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
-import { DICT_KEY } from '$env/static/private'
-import { translate } from '$lib/translate.server'
+import { getPhrase } from '$lib/api.server'
 
 export const GET: RequestHandler = async ({ url: { searchParams } }) => {
 	const phrase = searchParams.get('phrase')
 	const target = searchParams.get('target')
 	if (!phrase || phrase.length < 2 || !target) return json({ success: false })
 
-	return json({
-		def: await define(phrase),
-		translated: await translate(phrase, target)
-	} satisfies APIResponse)
-}
-
-async function define(phrase: string): Promise<WordDef[]> {
-	return fetch(
-		`https://www.dictionaryapi.com/api/v3/references/learners/json/${phrase}?key=${DICT_KEY}`
-	)
-		.then(res => res.json())
-		.then(res => (typeof res[0][0] !== 'string' ? res : []))
-		.catch(err => {
-			console.error('callAPI', err)
-			return []
-		})
+	return json(await getPhrase(phrase))
 }
