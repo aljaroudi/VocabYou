384e3abc4011596f4e0399d7c935536e58c73498
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2025-04-08 13:16:10 +0300

    feat: add define and getPhrase functions to enhance API capabilities for word definitions and translations

--- src/lib/api.server.ts
+++ src/lib/api.server.ts
@@ -1,5 +1,7 @@
 import { v2 as GCP } from '@google-cloud/translate'
-import { GOOGLE_APPLICATION_CREDENTIALS as key } from '$env/static/private'
+import { DICT_KEY, GOOGLE_APPLICATION_CREDENTIALS as key } from '$env/static/private'
+import { WordDefSchema } from './types'
+import type { WordDef } from './types-with-jsdoc'
 
 const translator = new GCP.Translate({ key })
 
@@ -15,3 +17,19 @@ export async function translate(text: string, target: string) {
 		.then(([translation]) => translation?.[0])
 		.catch(() => null)
 }
+
+export async function define(phrase: string): Promise<WordDef[]> {
+	return fetch(
+		`https://www.dictionaryapi.com/api/v3/references/learners/json/${phrase}?key=${DICT_KEY}`
+	)
+		.then(res => res.json())
+		.then(res => WordDefSchema.array().assert(res))
+		.catch(() => [])
+}
+
+export async function getPhrase(phrase: string) {
+	const [def, translated] = await Promise.all([define(phrase), translate(phrase, 'en')])
+	return { def, translated }
+}
+
+export type APIResponse = Awaited<ReturnType<typeof getPhrase>>
